abc08c9cb4bf12429f2fd829c13db1bf6aad7e6f
Wilman Pabel Vasquez Garcia <wilmanpabel@gmail.com>
2022-03-03 11:01:01 -0500

    Thu Mar  3 11:01:01 -05 2022

--- provider.tf
+++ provider.tf
@@ -1,3 +1,13 @@
+terraform {
+  backend "azurerm" {
+    #resource_group_name   = "RG-SHAREDSVC-PROD-100-EU2-ARQSOP"
+    #storage_account_name  = "stsharedsvcprod100eu2"
+    #el container varia dependiendo la capa
+    #container_name        = "shared-services-dev"
+    key                   = "terraform_test.tfstate"
+  }
+}
+
 provider "azurerm" {
   features {}
   subscription_id = "d32248ab-7cca-4946-9991-f94c7ba40ebd"
