9832ddeeca001ef27a543f81f10b08988b110969
Miguel Aliaga <maliaga.pantoja@gmail.com>
2024-08-19 12:51:33 -0500

    adding provider info

--- infra/provider.tf
+++ infra/provider.tf
@@ -1,10 +1,11 @@
 terraform {
-  required_providers {
-    null = {
-      source = "hashicorp/null"
-      version = "3.2.2"
+    required_version = ">= 1.0" 
+    required_providers {
+        null = {
+            source = "hashicorp/null"
+            version = "3.2.2"
+        }
     }
-  }
 }
 
 provider "null" {
