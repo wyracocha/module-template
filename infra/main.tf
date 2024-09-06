0539effd3c16ec575ce588549326f4ceb1cabc5c
Miguel Aliaga <maliaga.pantoja@gmail.com>
2024-09-06 09:45:33 -0500

    updating pipeline: adding conditions to stage security

--- infra/main.tf
+++
@@ -1,8 +0,0 @@
-resource "null_resource" "example" {
-  provisioner "local-exec" {
-    command = "echo This command will execute whenever the configuration changes"
-  }
-}
-output "hello_world" {
-  value = "Hello, World!"
-}
